f0cf708b999ed3ff053aea7af03b597f46d68452
Daniel Friesel <derf@finalrewind.org>
2018-09-24 16:17:00 +0200

    POSIX: Add loop and timer support

--- src/arch/posix/arch.cc
+++ src/arch/posix/arch.cc
@@ -1,12 +1,29 @@
 #include "arch.h"
 #include <unistd.h>
 
+#if defined(WITH_LOOP) || defined(TIMER_S)
+#include "driver/uptime.h"
+void loop();
+#endif
+#ifdef WITH_WAKEUP
+void wakeup();
+#endif
+
 void Arch::setup(void) { }
 
 void Arch::idle_loop(void)
 {
 	while (1) {
 		sleep(1);
+#ifdef WITH_LOOP
+		loop();
+#endif
+#ifdef WITH_WAKEUP
+		wakeup();
+#endif
+#ifdef TIMER_S
+		uptime.tick_s();
+#endif
 	}
 }
 
