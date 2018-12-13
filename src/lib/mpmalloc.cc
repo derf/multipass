ebd3e2890cad3fbc9b0293b91173995d191ee804
Daniel Friesel <derf@finalrewind.org>
2018-12-13 14:24:13 +0100

    mpmalloc

---
+++ src/lib/mpmalloc.cc
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "driver/stdout.h"
+#include "lib/mpmalloc.h"
+
+void* mpcalloc(size_t nmemb, size_t size) {
+	void* ret = calloc(nmemb, size);
+#ifdef ADDR_20BIT
+	kout << "calloc:" << dec << (uint32_t)nmemb << "x" << (uint32_t)size << "@" << ret << endl;
+#else
+	kout << "calloc:" << dec << nmemb << "x" << size << "@" << ret << endl;
+#endif
+	return ret;
+}
+
+void* mpmalloc(size_t size) {
+	void* ret = malloc(size);
+#ifdef ADDR_20BIT
+	kout << "malloc:" << dec << (uint32_t)size << "@" << ret << endl;
+#else
+	kout << "malloc:" << dec << size << "@" << ret << endl;
+#endif
+	return ret;
+}
+
+void mpfree(void* addr) {
+	kout << "free:" << addr << endl;
+	free(addr);
+}
