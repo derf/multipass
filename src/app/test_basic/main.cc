3fb252d01948f9fd78116e53a3da9c5b61c4a804
Daniel Friesel <derf@finalrewind.org>
2019-01-31 10:56:22 +0100

    add simple compile and option-combination tests for posix arch

---
+++ src/app/test_basic/main.cc
@@ -0,0 +1,31 @@
+#include "arch.h"
+#include "driver/gpio.h"
+#include "driver/stdout.h"
+#include "driver/uptime.h"
+
+void loop(void)
+{
+	kout << "Loop" << endl;
+	gpio.led_toggle(0);
+#ifdef TIMER_S
+	kout << dec << uptime.get_s() << endl;
+#endif
+}
+
+int main(void)
+{
+	arch.setup();
+	gpio.setup();
+	kout.setup();
+
+	/*
+	 * There may be some delay between program start and UART capture start.
+	 */
+	for (uint8_t i = 0; i < 255; i++) {
+		kout << "Hello, World!" << endl;
+	}
+
+	arch.idle_loop();
+
+	return 0;
+}
