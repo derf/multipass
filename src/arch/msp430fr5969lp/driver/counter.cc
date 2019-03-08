cd7ee540291f7412153ee9fa1e785cc859c589fe
Daniel Friesel <derf@finalrewind.org>
2019-03-08 11:53:33 +0100

    msp430fr5969 counter: Track overflows

--- src/arch/msp430fr5969lp/driver/counter.cc
+++ src/arch/msp430fr5969lp/driver/counter.cc
@@ -1,7 +1,18 @@
+#include "arch.h"
 #include "driver/counter.h"
+#include "driver/gpio.h"
 
 #if defined(TIMER_CYCLES)
 #warn "timer_cycles and counter are mutually exclusive. Expect odd behaviour."
 #endif
 
 Counter counter;
+
+__attribute__((interrupt(TIMER2_A1_VECTOR))) void handle_timer2_overflow()
+{
+	if (TA2IV == 0x0e) {
+		if (counter.overflow < 255) {
+			counter.overflow++;
+		}
+	}
+}
