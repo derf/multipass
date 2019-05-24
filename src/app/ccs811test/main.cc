ef918672a6415f862fa7b44d185088eee1816b7c
Daniel Friesel <daniel.friesel@uos.de>
2019-05-24 09:09:58 +0200

    add dysfunctional ccs811 test app

---
+++ src/app/ccs811test/main.cc
@@ -0,0 +1,34 @@
+#include "arch.h"
+#include "driver/gpio.h"
+#include "driver/stdout.h"
+#if defined(MULTIPASS_ARCH_HAS_I2C) && !defined(DRIVER_SOFTI2C)
+#include "driver/i2c.h"
+#else
+#include "driver/soft_i2c.h"
+#endif
+#include "driver/ccs811.h"
+
+void loop(void)
+{
+	kout << "CCS811 status is " << ccs811.check() << endl;
+}
+
+int main(void)
+{
+
+	arch.setup();
+	gpio.setup();
+	kout.setup();
+
+	if (i2c.setup() != 0) {
+		return 1;
+	}
+
+	kout << "I2C setup OK" << endl;
+
+	ccs811.init();
+
+	arch.idle_loop();
+
+	return 0;
+}
