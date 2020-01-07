f3cc28296c7cf60b904357dfb97571a67b1f6b98
Daniel Friesel <daniel.friesel@uos.de>
2020-01-07 09:44:40 +0100

    comment

--- src/app/luxlog/main.cc
+++ src/app/luxlog/main.cc
@@ -10,7 +10,7 @@
 #include "driver/max44009.h"
 
 // Alle 10 Minuten ein Messwert -> 144 Werte pro Tag -> 28,4 Tage für 4096 Werte
-// Alle 4 Minuten ein Messwert -> 144 Werte pro Tag -> 11,37 Tage für 4096 Werte
+// Alle 4 Minuten ein Messwert -> 360 Werte pro Tag -> 11,37 Tage für 4096 Werte
 __attribute__ ((section(".text"))) uint32_t log[4096];
 
 uint16_t log_offset = 0;
