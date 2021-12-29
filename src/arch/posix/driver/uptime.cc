9318c681f5e198d9541a3259066f415dbd351af0
Daniel Friesel <derf@finalrewind.org>
2021-12-29 17:47:56 +0100

    POSIX: Fix uptime.get_us()

--- src/arch/posix/driver/uptime.cc
+++ src/arch/posix/driver/uptime.cc
@@ -17,7 +17,7 @@ uint64_t Uptime::get_us()
 {
 	struct timespec ts;
 	clock_gettime(CLOCK_REALTIME, &ts);
-	return ts.tv_nsec / 1000;
+	return (uint64_t)ts.tv_sec * 1000000 + ts.tv_nsec / 1000;
 }
 
 uint64_t Uptime::get_cycles()
