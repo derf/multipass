d3c494bb95628b7988b4d4563c0acd9f62aae052
Birte Kristina Friesel <derf@finalrewind.org>
2026-07-26 12:54:41 +0200

    adctest: also runs on msp430fr5994lp

--- src/app/adctest/main.cc
+++ src/app/adctest/main.cc
@@ -10,16 +10,25 @@
 
 void loop(void)
 {
+#ifdef CONFIG_arch_arduino_nano
 	uint16_t vcc = adc.getVCC_mV();
 	uint16_t temp = adc.getTemp_mdegC();
 
-	kout << "Voltage     " << vcc << " mv" << endl;
+	kout << "Voltage     " << vcc << " mV" << endl;
 	kout << "Temperature " << temp << " m°C" <<  endl;
 
 	for (uint8_t admux_sel = 0; admux_sel < 8; admux_sel++) {
 		kout << "ADC" << admux_sel << "        " << adc.getPin_mV(admux_sel, vcc) << " mV vs AVCC" << endl;
 		kout << "ADC" << admux_sel << "        " << adc.getPin_mV(admux_sel) << " mV vs 1.1V bandgap" << endl;
 	}
+#endif
+#ifdef CONFIG_arch_msp430fr5994lp
+	float vcc = adc.getVCC();
+	float temp = adc.getTemp();
+
+	kout << "Voltage     " <<  vcc << " V" << endl;
+	kout << "Temperature " << temp << " °C" <<  endl;
+#endif
 }
 
 int main(void)
